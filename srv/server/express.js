bded93e814da526ead0dc627ee2bd31e2458ddcf
haonanJia <haonan.jia@sap.com>
2019-10-18 22:00:39 -0700

    add release toggles

--- srv/server/express.js
+++ srv/server/express.js
@@ -3,6 +3,7 @@
 const bodyParser = require('body-parser')
 const express = require('express')
 const log = require('cf-nodejs-logging-support')
+const request = require("request-promise");
 
 module.exports = function (app) {
 	var logLevel = process.env.LOG_LEVEL || "debug";
@@ -33,4 +34,19 @@ module.exports = function (app) {
 	app.use(
 		HDBConn.middleware(hanaOptions.hana)
 	);
+
+	app.featureFlag = async function getFeatureFlag(flag)
+	{
+		var options = { 
+			method: 'GET',
+			url: 'https://feature-flags.cfapps.eu10.hana.ondemand.com/api/v2/evaluate/'+flag,
+			headers: 
+			{
+				Authorization: 'Basic c2Jzc19rcXh5b2J2dmFkYmpvNGF2ZXh3b2h1ZGQyejUvd2J4K25jdXNjaGdkZGl4dm9kN20ydnVidjlkYWR4aGR4eWNpeTZ1PTphYV9SYnQwL2dyblRjTFQrYkFQYzd6WituQ0kzY009'
+			} 
+		};
+		
+		var body = await request(options);
+		return JSON.parse(body).variation == 'true';
+	}
 };
